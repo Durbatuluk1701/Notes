f7119b9db5bb53adf0eef622d4892129d188b7b4
Will Thomas <30wthomas@gmail.com>
2023-02-09 09:09:57 -0600

    Adding local gitignore and updating Coq file

--- QuickChick_SF_Vol_4/QC.v
+++ QuickChick_SF_Vol_4/QC.v
@@ -1,4 +1,5 @@
 Require Import String.
+From QuickChick Require Import QuickChick.
 Open Scope string_scope.
 
 Class Show A : Type :=
@@ -16,3 +17,37 @@ constructor; intros b; destruct b eqn:B.
 - (* b = true *) apply "true".
 - (* b = false *) apply "false".
 Defined.
+
+Local Instance showStr : Show string.
+constructor. intros s; apply s.
+Defined.
+
+(* NOTE:
+  This is copied directly from Software Foundations 
+  in Volume 4 QuickChick > Typeclasses chapter 
+*)
+Fixpoint string_of_nat_aux (time n : nat) (acc : string) : string :=
+  let d := match (Nat.modulo n 10) with
+           | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4" | 5 => "5"
+           | 6 => "6" | 7 => "7" | 8 => "8" | _ => "9"
+           end in
+  let acc' := d ++ acc in
+  match time with
+    | 0 => acc'
+    | S time' =>
+      match (Nat.div n 10) with
+        | 0 => acc'
+        | n' => string_of_nat_aux time' n' acc'
+      end
+  end.
+Definition string_of_nat (n : nat) : string :=
+  string_of_nat_aux n n "".
+
+Local Instance showNat : Show nat :=
+{
+  show := string_of_nat
+}.
+
+Compute (show true).
+Compute (show "hello").
+Compute (show 103).
